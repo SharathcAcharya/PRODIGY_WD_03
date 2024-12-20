a7ec533d1c4f18a63249a35afbbef8ec4cfb8338
Sharath Acharya <sharathacharya363@gmail.com>
2024-12-20 10:30:50 +0530

    added pwa support

---
+++ ServiceWorker.js
@@ -0,0 +1,11 @@
+//Installing Service Worker
+self.addEventListener("install", event => {
+});
+
+//Fetching Service Worker
+self.addEventListener("fetch", event => {
+});
+
+//Activating Service Worker
+self.addEventListener("activate", event => {
+});
